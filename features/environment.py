a62a5508fb3e00651bd15d183bcbc4e88237af68
mahadevann <33186104+mahadevann@users.noreply.github.com>
2018-10-17 15:08:27 -0400

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,6 @@ def before_all(context):
     context.browser = webdriver.Remote(command_executor='http://docker.for.mac.localhost:4444/wd/hub',desired_capabilities=DesiredCapabilities.CHROME)
     context.browser.set_page_load_timeout(10)
     context.browser.implicitly_wait(10)
-    context.browser.maximize_window()
 
 def after_all(context):
     context.browser.quit()
