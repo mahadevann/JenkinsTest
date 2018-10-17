d656cbb44a2e62e1803680b42bbac19c1d5a844f
mahadevann <33186104+mahadevann@users.noreply.github.com>
2018-10-17 14:18:26 -0400

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def before_all(context):
-    context.browser = webdriver.Remote(command_executor='http://selenium-hub-96:4444/wd/hub',desired_capabilities=DesiredCapabilities.CHROME)
+    context.browser = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',desired_capabilities=DesiredCapabilities.CHROME)
     context.browser.set_page_load_timeout(10)
     context.browser.implicitly_wait(10)
     context.browser.maximize_window()
